829d9b22c61aebfb2a6bd95eeb92ff5f9b94606d
emmawolfkid <155752537+emmawolfkid@users.noreply.github.com>
2026-01-26 10:54:46 +0300

    Configure email using environment variables

--- cornelsimba/settings.py
+++ cornelsimba/settings.py
@@ -190,7 +190,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'emmawolfgang90@gmail.com'
-EMAIL_HOST_PASSWORD = 'piagxpvoatfnealy'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = 'Cornel Simba System <yourgmail@gmail.com>'
+DEFAULT_FROM_EMAIL = 'Cornel Simba System <emmawolfgang90@gmail.com>'
