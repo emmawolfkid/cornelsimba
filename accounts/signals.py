8cdc45aa778b0e6d83cb65e12d1e583493f563d2
emmawolfkid <155752537+emmawolfkid@users.noreply.github.com>
2026-01-27 21:42:12 +0300

    last update including the dahsboard/main.html

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
-from audit.utils import log_action
+from audit.utils import audit_log
+from django.db.models.signals import post_migrate
+from django.contrib.auth.models import Group
 
 @receiver(user_logged_in)
 def log_login(sender, request, user, **kwargs):
-    log_action(
+   audit_log(
         user=user,
         action='LOGIN',
         module='Authentication',
@@ -13,9 +15,26 @@ def log_login(sender, request, user, **kwargs):
 
 @receiver(user_logged_out)
 def log_logout(sender, request, user, **kwargs):
-    log_action(
+    audit_log(
         user=user,
         action='LOGOUT',
         module='Authentication',
         description='User logged out'
     )
+
+# 👇 your system-wide groups (CONSTANT)
+DEFAULT_GROUPS = [
+    'Admin',
+    'Manager',
+    'HR',
+    'Finance',
+    'Inventory',
+    'Procurement',
+    'Sales',
+    'Auditor',
+]
+
+@receiver(post_migrate)
+def create_default_groups(sender, **kwargs):
+    for group_name in DEFAULT_GROUPS:
+        Group.objects.get_or_create(name=group_name)
